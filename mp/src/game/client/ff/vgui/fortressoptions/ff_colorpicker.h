862a0d703b0dda0fb7d22541a4c3cc8ef297685c
Adam Willden <adam.willden@dataflexnet.com>
2025-10-22 19:27:25 +0100

    core

---
+++ mp/src/game/client/ff/vgui/fortressoptions/ff_colorpicker.h
@@ -0,0 +1,67 @@
+#ifndef FF_COLORPICKER_H
+#define FF_COLORPICKER_H
+
+#include <vgui_controls/Button.h>
+#include <vgui_controls/ComboBox.h>
+#include <vgui_controls/ImagePanel.h>
+#include <vgui_controls/Panel.h>
+#include "ff_inputslider.h"
+
+namespace vgui
+{
+	class FFColorPicker : public Panel
+	{
+		DECLARE_CLASS_SIMPLE(FFColorPicker, Panel);
+	public:
+		FFColorPicker(Panel* parent, const char* name, Panel* pActionSignalTarget = NULL, bool bColorModeIntensity = false);
+
+		int GetRedComponentValue();
+		int GetGreenComponentValue();
+		int GetBlueComponentValue();
+		int GetAlphaComponentValue();
+		int GetColorMode();
+
+		void SetRedComponentValue(int iRed);
+		void SetGreenComponentValue(int iGreen);
+		void SetBlueComponentValue(int iBlue);
+		void SetAlphaComponentValue(int iAlpha);
+		void SetColorMode(int iColorMode);
+
+		void SetValue(int iRed, int iGreen, int iBlue, int iAlpha);
+		void GetValue(int& iRed, int& iGreen, int& iBlue, int& iAlpha);
+
+	private:
+		ImagePanel* m_pColorBackground;
+		Button* m_pDialogButton;
+
+		CFFInputSlider* m_pRed;
+		CFFInputSlider* m_pGreen;
+		CFFInputSlider* m_pBlue;
+		CFFInputSlider* m_pAlpha;
+		ComboBox* m_pColorMode;
+
+		int m_iTeamColorPreview;
+		int m_iRed;
+		int m_iGreen;
+		int m_iBlue;
+		int m_iAlpha;
+		int m_iColorMode;
+
+		virtual void ApplySchemeSettings(vgui::IScheme* pScheme);
+
+		void UpdateColor();
+		void RecalculateColorPreview();
+		void SetRGBComponentsEnabled(bool bState);
+
+		void OnTick(void);
+
+		MESSAGE_FUNC_PARAMS(OnRedComponentChanged, "RedComponentChanged", data);
+		MESSAGE_FUNC_PARAMS(OnGreenComponentChanged, "GreenComponentChanged", data);
+		MESSAGE_FUNC_PARAMS(OnBlueComponentChanged, "BlueComponentChanged", data);
+		MESSAGE_FUNC_PARAMS(OnAlphaComponentChanged, "AlphaComponentChanged", data);
+		MESSAGE_FUNC_PARAMS(OnColorModeChanged, "ColorModeChanged", data);
+		MESSAGE_FUNC_PARAMS(OnUpdateSliders, "SliderMoved", data);
+		MESSAGE_FUNC_PARAMS(OnUpdateCombos, "TextChanged", data);
+	};
+};
+#endif
